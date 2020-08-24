9b39ee664a7644946208b372936c6f24f382ecf5
Zhaodong Tian <v-zhtian@microsoft.com>
2020-08-23 23:36:33 -0700

    Add .netcore client sample

---
+++ src/System.Private.ServiceModel/tests/Stress/DotnetCoreSample/SampleClient/ConsoleApp5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.SayHelloClient client = new ServiceReference1.SayHelloClient();
+           var result= client.HelloAsync("helloworld");
+            Console.WriteLine(result.Result);
+        }
+    }
+}
