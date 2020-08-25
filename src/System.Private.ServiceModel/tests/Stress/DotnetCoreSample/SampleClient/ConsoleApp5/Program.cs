5000c8b69d6d16dd3937da7ca5be8640e78b4cf8
Zhaodong Tian <v-zhtian@microsoft.com>
2020-08-24 22:48:55 -0700

    update

--- src/System.Private.ServiceModel/tests/Stress/DotnetCoreSample/SampleClient/ConsoleApp5/Program.cs
+++ src/System.Private.ServiceModel/tests/Stress/DotnetCoreSample/SampleClient/ConsoleApp5/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            ServiceReference1.SayHelloClient client = new ServiceReference1.SayHelloClient();
+            System.ServiceModel.BasicHttpBinding binding = new System.ServiceModel.BasicHttpBinding();
+            binding.MaxBufferSize = int.MaxValue;
+            binding.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+            binding.MaxReceivedMessageSize = int.MaxValue;
+            binding.AllowCookies = true;
+           var address= new System.ServiceModel.EndpointAddress(args[0]);
+            ServiceReference1.SayHelloClient client = new ServiceReference1.SayHelloClient(binding,address);
            var result= client.HelloAsync("helloworld");
             Console.WriteLine(result.Result);
         }
