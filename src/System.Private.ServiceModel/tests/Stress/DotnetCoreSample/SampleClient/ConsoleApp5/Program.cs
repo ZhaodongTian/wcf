458c6845e1b23857aeb2246c9d8252898df77790
Zhaodong Tian <v-zhtian@microsoft.com>
2020-08-25 20:31:20 -0700

    Measure rps

--- src/System.Private.ServiceModel/tests/Stress/DotnetCoreSample/SampleClient/ConsoleApp5/Program.cs
+++ src/System.Private.ServiceModel/tests/Stress/DotnetCoreSample/SampleClient/ConsoleApp5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Xml.Schema;
 using Benchmarks;
 
 namespace ConsoleApp5
@@ -24,6 +25,21 @@ namespace ConsoleApp5
             BenchmarksEventSource.Log.Metadata("http/firstrequest", "max", "max", "First Request (ms)", "Time to first request in ms", "n0");
             BenchmarksEventSource.Measure("http/firstrequest", elapsed);
             Console.WriteLine(result.Result);
+
+            var startTime = DateTime.Now;
+            int request = 0;
+            while (DateTime.Now <= startTime.AddMinutes(2))
+            {
+                client.HelloAsync("helloworld");
+                request++;
+            }
+            Console.WriteLine(request);
+            BenchmarksEventSource.Log.Metadata("bombardier/requests", "max", "sum", "Requests", "Total number of requests", "n0");
+            BenchmarksEventSource.Measure("bombardier/requests", request);
+
+            BenchmarksEventSource.Log.Metadata("bombardier/rps/max", "max", "sum", "Requests/sec (max)", "Max requests per second", "n0");
+
+            BenchmarksEventSource.Measure("bombardier/rps/max", request/120);
         }
     }
 }
