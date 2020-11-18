9560cf20df60f8f1a0a259d176dea9081d29223b
Zhaodong Tian <v-zhtian@microsoft.com>
2020-11-17 17:55:04 -0800

    update

--- src/System.Private.ServiceModel/tests/Benchmarks/WCFCorePerf/WCFCorePerfService/Program.cs
+++ src/System.Private.ServiceModel/tests/Benchmarks/WCFCorePerf/WCFCorePerfService/Program.cs
@@ -8,8 +8,14 @@ using System.Threading.Tasks;
 
 namespace WCFCorePerfService
 {
+    public class Parameters
+    {
+        public const string Port = "port";
+    }
+
     class Program
     {
+        private string _paramPort = "Port";
         static async Task Main(string[] args)
         {
             string url = "https://wcfcrank.blob.core.windows.net/app/WcfCorePerfCrankService.zip";
@@ -18,23 +24,20 @@ namespace WCFCorePerfService
             var _httpClient = new HttpClient();
 
             using (var downloadStream = await _httpClient.GetStreamAsync(url))
-
-            using (var fileStream = File.Create(bombardierFileName))
             {
-                await downloadStream.CopyToAsync(fileStream);
+                using (var fileStream = File.Create(bombardierFileName))
+                {
+                    await downloadStream.CopyToAsync(fileStream);
+                }
             }
 
-            ZipFile.ExtractToDirectory(bombardierFileName, @".\",true);
-            //if (test.ProcessRunOptions(args))
-            //{
-            //    BenchmarksEventSource.Log.Metadata("channelopen", "max", "max", "Channel Open Time (ms)", "Time to Open Channel in ms", "n0");
-            //    BenchmarksEventSource.Log.Metadata("firstrequest", "max", "max", "First Request (ms)", "Time to first request in ms", "n0");
-            //    BenchmarksEventSource.Log.Metadata("bombardier/requests", "max", "sum", "Requests (" + test._paramPerfMeasurementDuration * 1000 + " ms)", "Total number of requests", "n0");
-            //    BenchmarksEventSource.Log.Metadata("bombardier/rps/max", "max", "sum", "Requests/sec (max)", "Max requests per second", "n0");
-
-            var process = new Process()
+            ZipFile.ExtractToDirectory(bombardierFileName, @".\", true);
+            Program test = new Program();
+            if (test.ProcessRunOptions(args))
             {
-                StartInfo = {
+                var process = new Process()
+                {
+                    StartInfo = {
                     FileName = "WcfCorePerfCrankService.exe",
                     RedirectStandardOutput = true,
                     RedirectStandardInput = true,
@@ -42,9 +45,7 @@ namespace WCFCorePerfService
                 },
                     EnableRaisingEvents = true
                 };
-
                 var stringBuilder = new StringBuilder();
-
                 process.OutputDataReceived += (_, e) =>
                 {
                     if (e != null && e.Data != null)
@@ -58,20 +59,37 @@ namespace WCFCorePerfService
                     }
                 };
 
-                //string arg = "binding:" + test._paramBinding + " transfermode:" + test._paramTransferMode + " perfmeasurementduration:" + test._paramPerfMeasurementDuration + " serviceurl:" + test._paramServiceUrl;
-               // process.StartInfo.Arguments = arg;
+                string arg = "port:" + test._paramPort;
+                process.StartInfo.Arguments = arg;
                 process.Start();
-               // BenchmarksEventSource.SetChildProcessId(process.Id);
                 process.BeginOutputReadLine();
-           // process.StandardInput.WriteLine();
-               process.WaitForExit();
+                process.WaitForExit();
+            }
+        }
 
-                //var dict = test.ProcessOutPut(stringBuilder.ToString());
-                //BenchmarksEventSource.Measure("channelopen", dict["channelopen"]);
-                //BenchmarksEventSource.Measure("firstrequest", dict["firstrequest"]);
-                //BenchmarksEventSource.Measure("bombardier/requests", dict["bombardier/requests"]);
-                //BenchmarksEventSource.Measure("bombardier/rps/max", dict["bombardier/rps/max"]);
-            //}
+        private bool ProcessRunOptions(string[] args)
+        {
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+                string[] p = s.Split(new char[] { ':' }, count: 2);
+                if (p.Length != 2)
+                {
+                    continue;
+                }
+
+                switch (p[0].ToLower())
+                {
+                    case Parameters.Port:
+                        _paramPort = p[1];
+                        break;
+                    default:
+                        Console.WriteLine("unknown argument: " + s);
+                        continue;
+                }
+            }
+
+            return true;
         }
     }
 }
